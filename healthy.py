ff06582de2dbce5dd81eacb2066f0f2c2433720f
Alexis Puente <alexispuente02@gmail.com>
2021-11-13 17:11:49 -0600

    added file for healthy tranformations

---
+++ healthy.py
@@ -0,0 +1,19 @@
+
+
+#Basic Lists
+recipe_categories = ["Appetizer and Snacks", "BBQ & Grilling", "Bread Recipes", "Breakfast and Brunch", "Desserts", "Dinner Recipes", "Drinks", "Everyday Cooking", "Fruits, Vegetables, and Other Produce", "Holidays and Events", "Ingredients", "Lunch Recipes", "Main Dishes", "Meat and Poultry", "Pasta and Noodles", "Salad Recipes", "Seafood Recipes", "Side Dishes", "Soups, Stews, and Chili", "Trusted Brands", "U.S. Recipes", "World Cuisine"]
+healthy_cooking_methods = ["Braising", "Broiling", "Grilling", "Poaching", "Sauteing", "Steaming"]
+
+def make_healthy(recipe):
+    #Change recipe here
+    healthy_recipe = recipe
+    return healthy_recipe
+
+def make_unhealthy(recipe):
+    #Change recipe here
+    unhealthy_recipe = recipe
+    return unhealthy_recipe
+
+
+if __name__ == '__main__':
+    None

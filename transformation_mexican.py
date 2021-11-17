a5f4c80072b32b3e521b9383e18ab3ba67368247
rtl9148 <robertloza2021@u.northwestern.edu>
2021-11-17 16:19:26 -0600

    added transformation reverses and updated lists using ingredient list

---
+++ transformation_mexican.py
@@ -0,0 +1,38 @@
+
+###Things that can be substituted
+##tortilla, lime, 
+
+###things that can be added
+
+
+##substitutions
+
+mexican_substitute = {"cinnamon" : ["nutmeg", "cloves", "ginger"],
+                        "oregano" : ["thyme", "marjoram"],
+                        "cilantro" : ["coriander", "curry powder", "garam masala","caraway", "parsley","basil"], ##"lettuce","romaine lettuce", "cabbage"],
+                        "cumin" : ["caraway seeds", "black cumin"],
+                        "paprika" : ["cayenne pepper", "chili powder"],
+                        "black beans" : [ "black eyed peas", "black-eyed peas", "garbanzo beans", "kidney beans", "lima beans"],
+                        "brown rice" : ["white rice", "jasmine rice", "basmati rice" "wild rice"], 
+                        "taco seasoning" : []
+                        
+}
+                        
+##list of additives
+mexican_add = [ "cilantro" , "pinto beans", "lime", 'salsa', 'garlic powder', 'onion'] ##onion needs to be chopped
+
+##modifiers
+
+
+##--------------TRANSFORM BACK LISTS
+mexican_substitute_rev = {
+                        "bread" : ["tortillas"], ##corn or flour tortillas?? also could use lettuce instead of tortilla
+                        "breakfast sausage" : ["chorizo", "chorizo sausage"],
+                        "shredded pork" : ["carnita", "carnitas"],
+                        ""
+                        
+}
+
+
+##spices
+# cumin,  paprika, chili powder(ancho?),garlic powder, onion powder,
---
+++ transformation_vegetarian.py
@@ -0,0 +1,204 @@
+
+##---------DICTIONARY RESPONSIBLE FOR TRANSFORMS FROM VEGETARIAN TO MEAT--------------------------------
+vegetarian_substitutes_rev = {}
+
+vegetarian_substitutes_rev['chicken broth'] = 'vegetable broth'
+vegetarian_substitutes_rev['beef broth'] = 'vegetable broth'
+
+##steak and steak cuts
+#steak(tenderloin, sirloin, filet mignon, flank, skirt, t-bone )
+vegetarian_substitutes_rev['steak'] = 'tofu'
+vegetarian_substitutes_rev['tenderloin'] = 'tofu'
+vegetarian_substitutes_rev['sirloin'] = 'tofu'
+vegetarian_substitutes_rev['filet mignon'] = 'tofu'
+vegetarian_substitutes_rev['flank'] = 'tofu'
+vegetarian_substitutes_rev['t-bone'] = 'tofu'
+
+##beef and beef types
+##hamburger(burger), prime rib, brisket, buffalo, oxtail
+vegetarian_substitutes_rev['beef'] = 'tofu'
+vegetarian_substitutes_rev['hamburger'] = 'seitan' ##beans, portabello mushrooms
+vegetarian_substitutes_rev['hamburgers'] = 'seitan'
+vegetarian_substitutes_rev['cheeseburger'] = 'seitan' ##beans, portabello mushrooms
+vegetarian_substitutes_rev['cheeseburgers'] = 'seitan'
+vegetarian_substitutes_rev['burger'] = 'seitan'
+vegetarian_substitutes_rev['prime rib'] = 'seitan'
+vegetarian_substitutes_rev['ribs'] = 'seitan'
+vegetarian_substitutes_rev['rib'] = 'seitan'
+vegetarian_substitutes_rev['brisket'] = 'seitan'
+vegetarian_substitutes_rev['buffalo'] = 'seitan'
+vegetarian_substitutes_rev['oxtail'] = 'beans'
+#meatloaf, meatballs
+vegetarian_substitutes_rev['meat'] = 'beans' 
+
+##chicken and chicken types
+#feet?
+vegetarian_substitutes_rev['chicken'] = 'tofu' ##bird, pheasant
+vegetarian_substitutes_rev['chicken breast'] = 'tofu' ##bird, pheasant
+vegetarian_substitutes_rev['breast'] = 'tofu' ##bird, pheasant
+vegetarian_substitutes_rev['chicken wings'] = 'tofu'
+vegetarian_substitutes_rev['wings'] = 'tofu'
+vegetarian_substitutes_rev['chicken thigh'] = 'tofu'
+vegetarian_substitutes_rev['chicken thighs'] = 'tofu'
+vegetarian_substitutes_rev['thighs'] = 'tofu'
+vegetarian_substitutes_rev['chicken leg'] = 'tofu'
+vegetarian_substitutes_rev['chicken legs'] = 'tofu'
+vegetarian_substitutes_rev['chicken drumstick'] = 'tofu'
+vegetarian_substitutes_rev['chicken drumsticks'] = 'tofu'
+vegetarian_substitutes_rev['chicken nuggets'] = 'tofu'
+vegetarian_substitutes_rev['chicken nugget'] = 'tofu'
+
+##lunch meats/sliced meats
+vegetarian_substitutes_rev['salami'] = 'mushroom'
+vegetarian_substitutes_rev['ham'] = 'seitan'
+vegetarian_substitutes_rev['turkey'] = 'seitan'
+vegetarian_substitutes_rev['bologna'] = 'mushroom'
+vegetarian_substitutes_rev['pepperoni'] = 'mushroom'
+
+##pork based foods
+vegetarian_substitutes_rev['pork'] = 'tofu' ##jackfruit for pulled pork dishes
+vegetarian_substitutes_rev['bacon'] = 'tofu'
+vegetarian_substitutes_rev['hot dog'] = 'tofu'
+vegetarian_substitutes_rev['sausage'] = 'tofu'
+vegetarian_substitutes_rev['brat'] = 'tofu'
+vegetarian_substitutes_rev['kielbasa'] = 'tofu'
+vegetarian_substitutes_rev['cutlet'] = 'tofu'
+
+##other meats
+vegetarian_substitutes_rev['duck'] = 'tofu'
+#falafel, gyro, venison, lamb
+vegetarian_substitutes_rev['goat'] = 'tofu'
+vegetarian_substitutes_rev['falafel'] = 'tofu'
+vegetarian_substitutes_rev['liver'] = 'tofu'
+vegetarian_substitutes_rev['gyro'] = 'tofu'
+vegetarian_substitutes_rev['venison'] = 'tofu'
+vegetarian_substitutes_rev['lamb'] = 'tofu'
+
+##fish
+vegetarian_substitutes_rev['salmon'] = 'tofu'
+vegetarian_substitutes_rev['cod'] = 'tofu'
+vegetarian_substitutes_rev['black cod'] = 'tofu'
+vegetarian_substitutes_rev['atlantic cod'] = 'tofu'
+vegetarian_substitutes_rev['herring'] = 'tofu'
+vegetarian_substitutes_rev['mahi-mahi'] = 'tofu'
+vegetarian_substitutes_rev['perch'] = 'tofu'
+vegetarian_substitutes_rev['trout'] = 'tofu'
+vegetarian_substitutes_rev['sardine'] = 'tofu'
+vegetarian_substitutes_rev['anchovy'] = "mushroom"
+vegetarian_substitutes_rev['bass'] = 'tofu'
+vegetarian_substitutes_rev['tuna'] = 'tofu'
+vegetarian_substitutes_rev['dogtooth tuna'] = 'tofu'
+vegetarian_substitutes_rev['bluefin tuna'] = 'tofu'
+vegetarian_substitutes_rev['bigeye tuna'] = 'tofu'
+vegetarian_substitutes_rev['albacore tuna'] = 'tofu'
+vegetarian_substitutes_rev['bluefish'] = 'tofu'
+vegetarian_substitutes_rev['butter fish'] = 'tofu'
+vegetarian_substitutes_rev['cat fish'] = 'tofu'
+vegetarian_substitutes_rev['catfish'] = 'tofu'
+vegetarian_substitutes_rev['shrimp'] = 'tofu'
+vegetarian_substitutes_rev['cray fish'] = 'tofu'
+vegetarian_substitutes_rev['crab'] = 'tofu'
+vegetarian_substitutes_rev['crabmeat'] = 'tofu'
+vegetarian_substitutes_rev['lobster'] = 'tofu'
+vegetarian_substitutes_rev['clam'] = 'tofu'
+vegetarian_substitutes_rev['octopus'] = 'tofu'
+vegetarian_substitutes_rev['squid'] = 'tofu'
+vegetarian_substitutes_rev['eel'] = 'tofu'
+
+vegetarian_substitutes_rev['dogfish'] = 'tofu'
+vegetarian_substitutes_rev['elk'] = 'tofu'
+vegetarian_substitutes_rev['fish'] = 'tofu'
+vegetarian_substitutes_rev['flounder'] = 'tofu'
+vegetarian_substitutes_rev['fly-fish'] = 'tofu'
+vegetarian_substitutes_rev['goose'] = 'tofu'
+vegetarian_substitutes_rev['mackrel'] = 'tofu'
+vegetarian_substitutes_rev['monkfish'] = 'tofu'
+vegetarian_substitutes_rev['mussel'] = 'tofu'
+vegetarian_substitutes_rev['ostrich'] = 'tofu'
+vegetarian_substitutes_rev['emu'] = 'tofu'
+vegetarian_substitutes_rev['oyster'] = 'tofu'
+vegetarian_substitutes_rev['pacific cod'] = 'tofu'
+vegetarian_substitutes_rev['pacific sanddab'] = 'tofu'
+vegetarian_substitutes_rev['pacific snapper'] = 'tofu'
+vegetarian_substitutes_rev['parrotfish'] = 'tofu'
+vegetarian_substitutes_rev['patagonian toothfish'] = 'tofu'
+vegetarian_substitutes_rev['perch'] = 'tofu'
+vegetarian_substitutes_rev['rock cod'] = 'tofu'
+vegetarian_substitutes_rev['rockfish'] = 'tofu'
+vegetarian_substitutes_rev['sablefish'] = 'tofu'
+vegetarian_substitutes_rev['scallop'] = 'tofu'
+vegetarian_substitutes_rev['sea bass'] = 'tofu'
+vegetarian_substitutes_rev['sea cucumber'] = 'tofu'
+vegetarian_substitutes_rev['sea urchin '] = 'tofu'
+vegetarian_substitutes_rev['shark'] = 'tofu'
+vegetarian_substitutes_rev['shellfish'] = 'tofu'
+vegetarian_substitutes_rev['snail'] = 'tofu'
+vegetarian_substitutes_rev['snapper'] = 'tofu'
+vegetarian_substitutes_rev['sturgeon'] = 'tofu'
+vegetarian_substitutes_rev['swordfish'] = 'tofu'
+vegetarian_substitutes_rev['tilapia'] = 'tofu'
+vegetarian_substitutes_rev['tunafish'] = 'tofu'
+vegetarian_substitutes_rev['yellowfin tuna'] = 'tofu'
+vegetarian_substitutes_rev['whitefish'] = 'tofu'
+
+##--------------------TRANSFORMATION FROM MEATS TO VEGETARIAN
+
+# pacific snapper, parrotfish, patagonian toothfish, perch, rock cod, rockfish, sablefish, scallop, sea bass, sea cucumber, sea urchin, sea scallop, shark, shellfish, snail, snapper, sturgeon, swordfish, tilapia, tuna fish, yellowfin tuna, whitefish
+### elk, fish stock, fish, flounder, fly-fish, goat, goose, lingcod, mackrel, monkfish, mussel, ostrich, emu, oyster, pacific cod, pacific sanddab,
+##seitan for larger meats(brisket, )
+vegetarian_substitutes = {
+    "tofu" : ["chicken","chicken breast", "steak", "tenderloin", "sirloin", "filet mignon", "flank", "t-bone steak","beef", \
+                "buffalo wings", "chicken wings", "wings", "chicken thigh", "chicken thighs","thigh","thighs","chicken leg","chicken legs", "chicken drumstick","chicken drumsticks", "drumsticks", \
+                "chicken nuggets", "nuggets", "pork", "bacon", "hot dog", "sausage","brat","kielbasa","cutlet","duck","lamb","goat","falafel","liver","gyro","venison", \
+                "bluefish","butter fish","cat fish","catfish","dogfish","salmon","cod", "black cod","atlantic cod","black cod","herring","mahi-mahi","perch","trout","sardine","bass", \
+                "tuna", "albacore tuna", "bigeye tuna","bluefin tuna","dogtooth tuna","shrimp","crab", "crabmeat", "cray fish","lobster","clam","octopus","squid","eel", \
+                "elk", "fish", "flounder", "fly-fish", "goose", "lingcod", "mackrel", "monkfish", "mussel", "ostrich", "emu", "oyster", "pacific cod", "pacific sanddab", 
+                "pacific snapper", "parrotfish", "patagonian toothfish", "perch", "rock cod", "rockfish", "sablefish", "scallop", "sea bass", "sea cucumber", "sea urchin",\
+                "sea scallop", "shark", "shellfish", "snail", "snapper", "sturgeon", "swordfish", "tilapia", "tuna fish", "yellowfin tuna", "whitefish"],
+    "seitan" : [ "ham", "turkey","hamburger", "hamburgers", "cheeseburger", "cheeseburgers", "burger", "prime rib", "ribs", "rib","brisket" ],
+    "mushroom" : ["salami","pepperoni","bologna", "anchovy"],
+    "vegetable broth" : ["chicken broth", "beef broth"]
+}
+
+##gumbo? stew?
+###ears, shoulder, eyes, arm, body, thigh[s], butt, breast, carcass?,
+
+
+## https://www.allrecipes.com/recipe/221958/chef-johns-perfect-prime-rib/
+ex_ingredients = [{'name':'Prime Rib','quantity': 4.0,'measurement':'pounds','tools': '','methods' : 'roast'}, \
+                    {'name':'butter','quantity': 0.25,'measurement':'cup','tools': '','methods' : ''}, \
+                    {'name':'black pepper','quantity':1.0,'measurement':'tablespoon','tools': 'measuring cup','methods' : ''}, \
+                    {'name':'herbes de Provence','quantity':1.0,'measurement':'teaspoon','tools': 'measuring cup','methods' : ''}, \
+                    {'name':'Salt','quantity':0.0,'measurement':'','tools': '','methods' : 'kosher'}]
+
+## https://www.allrecipes.com/recipe/20963/oven-roasted-potatoes/
+ex_ingredients2 = [{'name':'olive oil','quantity': .125, 'measurement':'cup','tools': 'measuring cup','methods' : ''}, \
+                    {'name':'garlic','quantity': 1.0, 'measurement':'tablespoon','tools': 'knife','methods' : 'minced'}, \
+                    {'name':'basil','quantity': 0.5, 'measurement':'teaspoon','tools': 'measuring cup','methods' : 'dried'}, \
+                    {'name':'marjoram','quantity': 0.5, 'measurement':'teaspoon','tools': 'measuring cup','methods' : 'dried'}, \
+                    {'name':'dill weed','quantity': 0.5, 'measurement':'teaspoon','tools': 'measuring cup','methods' : 'dried'}, \
+                    {'name':'thyme','quantity': 0.5, 'measurement':'teaspoon','tools': 'measuring cup','methods' : 'dried'}, \
+                    {'name':'oregano','quantity': 0.5, 'measurement':'teaspoon','tools': 'measuring cup','methods' : 'dried'}, \
+                    {'name':'parsley','quantity': 0.5, 'measurement':'teaspoon','tools': 'measuring cup','methods' : 'dried'}, \
+                    {'name':'red pepper flakes','quantity': 0.5, 'measurement':'teaspoon','tools': 'measuring cup','methods' : 'crushed'}, \
+                    {'name':'salt','quantity': 0.5, 'measurement':'teaspoon','tools': 'measuring cup','methods' : ''}, \
+                    {'name':'potatoes','quantity': 4, 'measurement':'count','tools': ['peeler', 'knife'],'methods' : ['peeled', 'cubed']}]
+
+##second list of get rid of things
+##Get rid of sugar, etc
+
+##This function will take in a list of ingredients and tranform them to contain only 
+##vegetarian products
+def transform_vegetarian(ingredients) :
+    if len(ingredients) < 2:
+        ##only have to transform first one
+        x = 1
+    list_vegetarian_subs = vegetarian_substitutes.keys()
+    for ing in ingredients :
+        if ing['name'] in list_vegetarian_subs :
+            ##substitute ingredient name
+            substitute(ingredient)
+        ##if ingredient is in reducible_ingreidents:
+            ##reduce amt
+
+

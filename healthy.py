1131f22219604deb7cb93bc70f8ef4673116ff28
Alexis Puente <alexispuente02@gmail.com>
2021-11-19 16:56:35 -0600

    updated healthy transformations

--- healthy.py
+++ healthy.py
@@ -1,29 +1,35 @@
 
-#Basic Lists
-recipe_categories = ["Appetizer and Snacks", "BBQ & Grilling", "Bread Recipes", "Breakfast and Brunch", "Desserts", "Dinner Recipes", "Drinks", "Everyday Cooking", "Fruits, Vegetables, and Other Produce", "Holidays and Events", "Ingredients", "Lunch Recipes", "Main Dishes", "Meat and Poultry", "Pasta and Noodles", "Salad Recipes", "Seafood Recipes", "Side Dishes", "Soups, Stews, and Chili", "Trusted Brands", "U.S. Recipes", "World Cuisine"]
-healthy_cooking_methods = ["Braising", "Broiling", "Grilling", "Poaching", "Sauteing", "Steaming"]
 
 ########### HEALTHY SUBSTITUTIONS AND MODIFICATIONS ########### 
 to_healthy_substitution = {
     "whole-grain brown rice":  ["rice", "brown rice", "white rice", "long grain white rice", "uncooked instant rice", "instant rice", "wild rice", "uncooked white rice", "uncooked brown rice"],
     "skim milk":               ["milk", "whole milk", "heavy cream", "half-and-half", "evaporated milk", "2% milk", "heavy whipping cream", "buttermilk", "warm milk", "cold milk"],
     "light margarine":         ["butter", "shortening", "margarine", "salted butter", "unsalted butter", "butter or margarine", "butter OR margarine"],
-    "dark chocolate chips":    ["chocolate chips", "milk chocolate chips", "chocolate morsels", "bittersweet chocolate chips"],
+    "dark chocolate chips":    ["chocolate chips", "milk chocolate chips", "chocolate morsels", "bittersweet chocolate chips", "BAKER'S Semi-Sweet Baking Chocolate, melted",],
     "vegetable stock":         ["chicken broth", "chicken stock", "beef stock", "beef broth", "chicken base"],
     "ground turkey":           ["ground beef", "lean ground beef", "ground pork", "ground veal"],
     "whole-wheat flour":       ["all-purpose flour", "flour", "enriched flour"],
     "chicken sausage":         ["sausage", "pork sausage", "bulk pork sausage"],
     "extra-virgin olive oil:": ["olive oil", "canola oil", "vegetable oil", "canola", "oil"], 
     "dark chocolate":          ["chocolate", "milk chocolate", "semi-sweet chocolate"],
+    "maple syrup":             ["corn syrup", "high fructose corn syrup", "light corn syrup",],
 }
 
 to_healthy_modification = {
-    "reduced fat": ["cheese", "Cheddar", "Cheddar cheese", "shredded Cheddar", "shredded sharp Cheddar cheese", "American cheese", "Swiss cheese", "bacon", "turkey bacon", "mozzarella", "shredded mozzarella", "cheddar",],
+    "reduced fat": ["cheese", "Cheddar", "Cheddar cheese", "shredded Cheddar", "shredded sharp Cheddar cheese", "American cheese", "Swiss cheese", \
+                    "bacon", "turkey bacon", "mozzarella", "shredded mozzarella", "cheddar", "feta", "feta cheese", "crumbled feta",],
     "fat free":    ["plain yogurt", "yogurt", "fruit yogurt", "sour cream", "greek yogurt"],
-    "sugar free":  ["ketchup", "granola", "juice", "licorice", ],
-    "low fat":     ["cream cheese", "cream cheese, softened", "mayonnaise", "mayo", "ice cream", ],
-    "whole grain": ["pasta", "spaghetti", "fettucine", "penne", "rigatoni", "bowtie", "farfalle", "linguine", "angel's hair", "rotini", "elbows", "macaroni", "orzo", "oat", "oats",],
-    "whole wheat": ["white bread", "croissant", "rolls" "brioche", "buns", "bagels", "thick slices bread", "ciabatta rolls", "French baguette", "baguette", "rye bread", "muffin"],
+    "sugar free":  ["ketchup", "granola", "juice", "licorice", "can peach pie filling", "peach pie filling", "barbecue sauce", "barbeque sauce", "whole cranberry sauce"], 
+    "low fat":     ["cream cheese", "cream cheese, softened", "mayonnaise", "mayo", "ice cream", "PHILADELPHIA Cream Cheese, softened", \
+                    "package PHILADELPHIA Cream Cheese, softened"],
+    "whole grain": ["pasta", "spaghetti", "fettucine", "penne", "rigatoni", "bowtie", "farfalle", "linguine", "angel's hair", "rotini", "elbows", \
+                    "macaroni", "orzo", "oat", "oats",],
+    "whole wheat": ["white bread", "croissant", "rolls" "brioche", "buns", "bagels", "thick slices bread", "ciabatta rolls", "French baguette", \
+                    "baguette", "rye bread", "muffin", "French bread"],
+    "low sodium":  ["fish sauce", "soy sauce", "balsamic vinaigrette", "miso paste", "pimento-stuffed green olives", "can chopped tomatoes in puree", \
+                    "creamy salad dressing", "cole slaw dressing", "jar sauerkraut with liquid", "beef bouillon", "cubes beef bouillon", "sauerkraut with liquid" \
+                    "condensed cream of mushroom soup", "can condensed cream of mushroom soup", "Italian-style salad dressing", "Bavarian-style sauerkraut, undrained", "balsamic vinaigrette salad dressing, or to taste", "condensed cream of celery soup", "dry brown gravy mix", "bottle soy sauce", "creamy salad dressing"],
+    "lean":        ["ham", "cooked ham", "chuck roast", "pork tenderloin", "rump roast", "thick cut pork chops", "pork loin roast", "pork chops", "ham hock", "chopped ham", "skinless chicken thighs", "cooked ham, crumbled"],
 }
 
 ########### UNHEALTHY SUBSTITUTIONS AND MODIFICATIONS ########### 
